bdf492be805ab13a99ff257653945749713fc98a
Ihor Klimov <ihor.klimov@delta.com>
2023-01-10 22:36:57 -0500

    Add task 3 for lab 2

---
+++ src/com/company/lab2/Group.java
@@ -0,0 +1,28 @@
+package com.company.lab2;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Group {
+    private final String groupName;
+    private final List<String> students;
+    private final Map<String, List<Integer>> grades = new HashMap<>();
+
+    public Group(String groupName, List<String> students) {
+        this.groupName = groupName;
+        this.students = students;
+    }
+
+    public synchronized void addGrade(String studentName, int grade) {
+        if (!grades.containsKey(studentName)) {
+            grades.put(studentName, new ArrayList<>());
+        }
+        grades.get(studentName).add(grade);
+    }
+
+    public List<String> getStudents() {
+        return students;
+    }
+}
